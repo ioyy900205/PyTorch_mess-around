f9a6e9c7241ba6696da502ded66b02ffdcb6ef7c
ioyy900205 <ioyy900205@gmail.com>
2021-06-02 14:42:22 +0800

    1

--- timm_test.py
+++ timm_test.py
@@ -1,12 +1,10 @@
 '''
 Date: 2021-06-02 10:58:25
 LastEditors: Liuliang
-LastEditTime: 2021-06-02 11:00:45
+LastEditTime: 2021-06-02 12:20:36
 Description: timm 使用      
 '''
 import timm
-
-import timm
 from pprint import pprint
-model_names = timm.list_models(pretrained=True)
+model_names = timm.list_models('*efficientnet_l2*')
 pprint(model_names)
